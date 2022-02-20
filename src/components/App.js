c64ef72ea78749a1ace7186556af0812d03ee3d0
MikeLeYeti <mikeleyeti@yahoo.fr>
2022-02-20 10:14:06 +0100

    Initialisation Conjugator

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import '../styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+        🏗️ Appli. de conjugaison en cours de construction 🏗️
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
