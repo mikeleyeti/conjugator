40af18c57f5cabc2fadadc72c5536ce19cba5db8
MikeLeYeti <mikeleyeti@yahoo.fr>
2022-02-20 11:01:36 +0100

    Structure de base

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,17 @@
 import '../styles/App.css';
+import Conjug from './Conjugator'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <p>
         🏗️ Appli. de conjugaison en cours de construction 🏗️
         </p>
-      </header>
+      </header> */}
+      <body>
+        <Conjug />
+      </body>
     </div>
   );
 }
---
+++ src/components/Conjugator.js
@@ -0,0 +1,10 @@
+import "../datas/conjugation-fr.xml"
+import "../datas/verbs-fr.xml"
+
+function Conjug() {
+    return(
+         "test"
+         )
+}
+
+export default Conjug
