786658decdfd1b9bf681ebf7851d2150b70aa9c5
MikeLeYeti <mikeleyeti@yahoo.fr>
2022-02-24 18:24:05 +0100

    Refactorisation du code sous forme de fcts

--- src/components/Conjugator.js
+++ src/components/Conjugator.js
@@ -3,6 +3,33 @@ import verbs from "../datas/verbs-fr.json"
 
 function Conjug() {
 
+  function listeVerbes() {
+    // Renvoie la liste des verbes du fichier "verbs-fr.json"
+    // Chaque élément de la liste est un objet constitué des clés
+    // infinitifs et modèles
+    const nbVerbes = verbs["verbs-fr"].v.length
+    // 7015 verbes au début du developpement
+    let liste_verbes = []
+    for (let index = 0; index < nbVerbes ; index++) {
+            const infinitif = verbs["verbs-fr"].v[index].i ;
+            const modele = verbs["verbs-fr"].v[index].t ;
+            let verbe = {"infinitif":infinitif,"modele":modele};            
+            liste_verbes.push(verbe)
+              }
+    return liste_verbes
+  }
+
+  const liste_verbes = listeVerbes();
+  // console.log(liste_verbes)
+
+  function choix_1_verbe(liste_verbes) {
+    return liste_verbes[Math.floor(Math.random()*liste_verbes.length)];
+  }
+
+  let verbe_alea = choix_1_verbe(liste_verbes)
+
+  console.log(verbe_alea)
+
   function chercherTemplate(verbe) {
     // Renvoie l'index du modèle `verbe` dans le tableau de conjugation
     let v = ""
@@ -14,32 +41,27 @@ function Conjug() {
     return index-1
   }
 
-  for (let index = 0; index < 10; index++) {
-    
-    
-    // Affiche les données
-    const [infinitif,modele] = [verbs["verbs-fr"].v[index].i,verbs["verbs-fr"].v[index].t]
-    console.log(infinitif,modele)
-     // Déterminer l'index où couper le verbe en suivant de modèle 
+  function conjuguer(verbe,temps) {
+    // Renvoir la liste des verbes conjugués au temps passé en paramètre
+
+    // Déterminer l'index où couper le verbe en suivant de modèle 
     // Par exemple aim:er 
-    const indexDeCoupe = modele.length - modele.indexOf(":") - 1
-    const radical = infinitif.slice(-infinitif.length,-indexDeCoupe)
-    console.log(infinitif,radical)
-    // Cherche le modèle de conjugaison
-    let indexConjugation = chercherTemplate(modele)
+    const indexDeCoupe = verbe.modele.length - verbe.modele.indexOf(":") - 1
+    const radical = verbe.infinitif.slice(-verbe.infinitif.length,-indexDeCoupe)
+
+  // Cherche le modèle de conjugaison
+    let indexConjugation = chercherTemplate(verbe.modele)
     let tabConjug = conjugation["conjugation-fr"].template[indexConjugation].indicative.present.p
     for (let index = 0; index < tabConjug.length; index++) {
       const terminaison = tabConjug[index].i[0];
       console.log(radical + terminaison)
+    }    
+    return null
+  }
 
-    }     
-    // todo : Rechercher le modèle dans "conjugation" en fonction du temps et appliquer les bonnes terminaisons
-  }  
-  
+  conjuguer(verbe_alea,"present")  
 
-  
-
-  
+  // todo : Rechercher le modèle dans "conjugation" en fonction du temps et appliquer les bonnes terminaisons
     return(
          "test"
          )
