59d969bce5e308e8431339cbd1218bbefb90cc00
MikeLeYeti <mikeleyeti@yahoo.fr>
2022-02-27 14:44:16 +0100

    Affichage verbe conjugué (RAW)

--- src/components/Conjugator.js
+++ src/components/Conjugator.js
@@ -1,18 +1,25 @@
 import { listeVerbes,choix_1_verbe,conjuguer,conjuguerAvecPronom , pronoms} from "../scripts/conjuguer" 
 
 function Conjug() {    
-  
+
   const liste_verbes = listeVerbes();
   // console.log(liste_verbes)
   let verbe_alea = choix_1_verbe(liste_verbes)
-  console.log(verbe_alea)
+  // console.log(verbe_alea)
   const verbe_conjugue = conjuguer(verbe_alea,"present")    
   const verbe_conjugue_avec_pronoms = conjuguerAvecPronom(pronoms,verbe_conjugue)
-  console.log(verbe_conjugue_avec_pronoms)
 
   // todo : Rechercher le modèle dans "conjugation" en fonction du temps et appliquer les bonnes terminaisons
     return(
-         "test"
+         <div>
+           <h2> Conjugaison de {verbe_alea.infinitif} </h2>
+           <ul>
+             {verbe_conjugue_avec_pronoms.map( (verbe) => (
+              <li key={verbe}> { verbe } </li>
+             )              
+             )}
+           </ul>
+         </div>
          )
 }
 
