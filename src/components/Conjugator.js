d62da78ad849a667ab657b747bffd96d73529dad
MikeLeYeti <mikeleyeti@yahoo.fr>
2022-03-06 13:38:57 +0100

    Affichage "propre" des temps

--- src/components/Conjugator.js
+++ src/components/Conjugator.js
@@ -6,6 +6,29 @@ import '../styles/Conjugator.css';
 
 function Conjug({verbe , updateVerbe , temps , updateTempsAffiché}) {    
 
+  function affichageTemps(tempsBrut) {
+    // Affichage en français du temps utilisé
+    let affichage
+    if (tempsBrut === "indicatif_present") {
+      affichage = "Présent de l'indicatif"
+    } else if (tempsBrut === "indicatif_imparfait") {
+      affichage = "Imparfait de l'indicatif"
+    } else if (tempsBrut === "indicatif_futur") {
+      affichage = "Futur de l'indicatif"
+    } else if (tempsBrut === "indicatif_passe_simple") {
+      affichage = "Passé simple de l'indicatif"
+    } else if (tempsBrut === "conditionnel_present") {
+      affichage = "Présent du conditionnel"
+    } else if (tempsBrut === "subjontif_present") {
+      affichage = "Présent du subjonctif"
+    } else if (tempsBrut === "subjontif_imparfait") {
+      affichage = "Imparfait du subjonctif"
+    } else if (tempsBrut === "imperatif_present") {
+      affichage = "Présent de l'impératif"
+    }
+    return affichage  
+  }
+
   const liste_verbes = listeVerbes();
   // Détermine l'index du verbe dans la liste des "objets" verbes
   let index_verbe = liste_verbes.findIndex( (obj) => obj.infinitif === verbe )
@@ -17,7 +40,7 @@ function Conjug({verbe , updateVerbe , temps , updateTempsAffiché}) {
   // todo : Rechercher le modèle dans "conjugation" en fonction du temps et appliquer les bonnes terminaisons
     return(
          <div className="Conjugator-bloc">
-           <h2> {temps} </h2>
+           <h2> {affichageTemps(temps)} </h2>
            <ul>
              {verbe_conjugue_avec_pronoms.map( (verbe) => (
               <li key={verbe}> { verbe } </li>
