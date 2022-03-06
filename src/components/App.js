ae126a636be614bc66d405aed948256e51986d8c
MikeLeYeti <mikeleyeti@yahoo.fr>
2022-03-06 19:03:59 +0100

    Choix verbe aléatoire + bouton tous temps

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import '../styles/App.css';
 import Conjug from './Conjugator'
 import ChoixTemps from "./ChoixTemps";
+import { choix_1_verbe , listeVerbes } from "../scripts/conjuguer";
 import { useState } from 'react'
 
 function App() {  
-  const [verbe, updateVerbe] = useState('aimer')
   // tempsAffichés est un dictionnaire dont les clés sont les temps dispo. et les valeurs des booléens décidant de l'affichage
-  const [tempsAffichés , updateTempsAffichés] = useState({'indicatif_present' : true,'indicatif_imparfait':false,'indicatif_futur':false,'indicatif_passe_simple':false,'conditionnel_present':false,'subjontif_present':false,'subjontif_imparfait':false,'imperatif_present':false})
+  const tempsParDefaut = {'indicatif_present' : true,'indicatif_imparfait':false,'indicatif_futur':false,'indicatif_passe_simple':false,'conditionnel_present':false,'subjontif_present':false,'subjontif_imparfait':false,'imperatif_present':false}
+  const [tempsAffichés , updateTempsAffichés] = useState(tempsParDefaut)
+  const [verbe, updateVerbe] = useState('aimer')
   // const [temps, updateTemps] = useState('indicatif_present')
 
   return (    
@@ -16,6 +18,8 @@ function App() {
         🏗️ Appli. de conjugaison en cours de construction 🏗️
         </p>
       </header>
+        <button onClick={() => (updateVerbe(choix_1_verbe(listeVerbes()).infinitif))}  > Choisir un verbe aléatoire </button>
+        <button onClick={() => (updateTempsAffichés({'indicatif_present' : true,'indicatif_imparfait':true,'indicatif_futur':true,'indicatif_passe_simple':true,'conditionnel_present':true,'subjontif_present':true,'subjontif_imparfait':true,'imperatif_present':true}))} > Tous </button>
       <form>
         {Object.entries(tempsAffichés).map( ([cle,valeur]) => ( <ChoixTemps temps={cle} tempsAffichés={tempsAffichés} updateTempsAffichés={updateTempsAffichés} key={cle}   />  ) )}        
       </form>
