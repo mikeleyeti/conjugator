252b47131e6268208154c3711742b1739f4fed53
MikeLeYeti <mikeleyeti@yahoo.fr>
2022-03-06 11:00:04 +0100

    Style

--- src/components/Conjugator.js
+++ src/components/Conjugator.js
@@ -1,4 +1,5 @@
 import { listeVerbes,conjuguer,conjuguerAvecPronom } from "../scripts/conjuguer" 
+import '../styles/Conjugator.css';
 
 // Temps disponibles :
 // const liste_temps = ['indicatif_present','indicatif_imparfait','indicatif_futur','indicatif_passe_simple','conditionnel_present','subjontif_present','subjontif_imparfait','imperatif_present' ]
@@ -15,8 +16,8 @@ function Conjug({verbe , updateVerbe , temps , updateTempsAffiché}) {
   const verbe_conjugue_avec_pronoms = conjuguerAvecPronom(verbe_conjugue)
   // todo : Rechercher le modèle dans "conjugation" en fonction du temps et appliquer les bonnes terminaisons
     return(
-         <div>
-           <h2> Conjugaison de {verbe.infinitif} au {temps} </h2>
+         <div className="Conjugator-bloc">
+           <h2> {temps} </h2>
            <ul>
              {verbe_conjugue_avec_pronoms.map( (verbe) => (
               <li key={verbe}> { verbe } </li>
