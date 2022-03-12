36ae6dd0c33f01d3e4a86364ab4dcd47f6a4edd2
MikeLeYeti <mikeleyeti@yahoo.fr>
2022-03-12 17:04:35 +0100

    Préparation des 3 modes

--- src/components/App.js
+++ src/components/App.js
@@ -9,15 +9,21 @@ function App() {
   const tempsParDefaut = {'indicatif_present' : true,'indicatif_imparfait':false,'indicatif_futur':false,'indicatif_passe_simple':false,'conditionnel_present':false,'subjontif_present':false,'subjontif_imparfait':false,'imperatif_present':false , 'passe_compose': false}
   const [tempsAffichés , updateTempsAffichés] = useState(tempsParDefaut)
   const [verbe, updateVerbe] = useState('aimer')
+  const [modeApplication, updateModeApplication] = useState('Conjugueur')
   // const [temps, updateTemps] = useState('indicatif_present')
   const liste_des_verbes = listeVerbes()
 
   function handleVerbe(event) {
+    // Verifie si le verbe est dans la liste des verbes sans tenir compte des espaces et majuscules
     let verbe = event.target.value.toLowerCase().trim()
     let index_verbe = liste_des_verbes.findIndex( (obj) => obj.infinitif === verbe )
     if (index_verbe !== -1) {
         updateVerbe(liste_des_verbes[index_verbe].infinitif)
     }
+  }
+
+  function handleModeApplication(event) {
+    updateModeApplication(event.target.id)
   }
 
   return (    
@@ -26,17 +32,25 @@ function App() {
         <p>
         🏗️ Appli. de conjugaison en cours de construction 🏗️
         </p>
+        <div>
+          <button id='Conjugueur' onClick={(event) => handleModeApplication(event)}> Conjugueur </button>
+          <button id='Cours' onClick={(event) => handleModeApplication(event)}> Cours </button>
+          <button id='Jeux' onClick={(event) => handleModeApplication(event)}> Jeux </button>
+        </div>
       </header>
-      <div>      
-        <button onClick={() => (updateVerbe(choix_1_verbe(listeVerbes()).infinitif))}  > Choisir un verbe aléatoire </button>
-        <button onClick={() => (updateTempsAffichés({'indicatif_present' : true,'indicatif_imparfait':true,'indicatif_futur':true,'indicatif_passe_simple':true,'conditionnel_present':true,'subjontif_present':true,'subjontif_imparfait':true,'imperatif_present':true}))} > Tous </button>
-        <input type="search" id="recherche-verbe" name="v" placeholder={verbe} onChange={(event) => handleVerbe(event)} />
+      {modeApplication === 'Conjugueur' ? 
+      <div id='Conjugueur'>
+        <div>      
+          <button onClick={() => (updateVerbe(choix_1_verbe(listeVerbes()).infinitif))}  > Choisir un verbe aléatoire </button>
+          <button onClick={() => (updateTempsAffichés({'indicatif_present' : true,'indicatif_imparfait':true,'indicatif_futur':true,'indicatif_passe_simple':true,'conditionnel_present':true,'subjontif_present':true,'subjontif_imparfait':true,'imperatif_present':true}))} > Tous </button>
+          <input type="search" id="recherche-verbe" name="v" placeholder={verbe} onChange={(event) => handleVerbe(event)} />
+        </div>
+        {Object.entries(tempsAffichés).map( ([cle,valeur]) => ( <ChoixTemps temps={cle} tempsAffichés={tempsAffichés} updateTempsAffichés={updateTempsAffichés} key={cle}   />  ) )}        
+        <div>
+        {Object.entries(tempsAffichés).map( ([cle,valeur]) => (valeur ? <Conjug verbe={verbe} key={cle+':'+verbe} updateVerbe={updateVerbe} temps={cle} updateTempsAffichés = {updateTempsAffichés}/> : null ) )}
+        </div> 
       </div>
-      {Object.entries(tempsAffichés).map( ([cle,valeur]) => ( <ChoixTemps temps={cle} tempsAffichés={tempsAffichés} updateTempsAffichés={updateTempsAffichés} key={cle}   />  ) )}        
-      <div>
-      {Object.entries(tempsAffichés).map( ([cle,valeur]) => (valeur ? <Conjug verbe={verbe} key={cle+':'+verbe} updateVerbe={updateVerbe} temps={cle} updateTempsAffichés = {updateTempsAffichés}/> : null ) )}
-      </div>
-      {/* <Conjug verbe="être"  updateVerbe={updateVerbe} temps={temps} updateTemps = {updateTemps}/> */}
+      : null }
     </div>
   );
 }
